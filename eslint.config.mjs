15b65cd09d82a161e8612bfaf1de5b9dce124a7d
Belinda Launer <launerb@gmail.com>
2025-06-12 02:51:29 +1000

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,21 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      
+    },
+  }),
+]
 
 export default eslintConfig;
